e89099e330c57e71fb5c00b3e9c82f74f7e06eb8
João Carlos Castro Mendes De Lima <jao@Joaos-Mac-mini.local>
2023-10-03 13:40:21 -0300

    Add ShoppingCart Badge

--- farmco/lib/src/home/home_tab.dart
+++ farmco/lib/src/home/home_tab.dart
@@ -1,5 +1,6 @@
 import 'package:farmco/src/config/custom_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -15,26 +16,47 @@ class HomeTab extends StatelessWidget {
         title: Text.rich(
           TextSpan(
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 30,
               fontWeight: FontWeight.bold,
               color: CustomColors.customSwatchColor,
             ),
-            children: const [
-              TextSpan(
+            children: [
+              const TextSpan(
                 text: 'Farm.',
                 style: TextStyle(),
               ),
               TextSpan(
                 text: 'CO2',
-                style: TextStyle(),
+                style: TextStyle(
+                  color: CustomColors.customContrastColor,
+                ),
               ),
             ],
           ),
         ),
+        actions: [
+          badges.Badge(
+            position: badges.BadgePosition.topEnd(top: 10, end: 50),
+            showBadge: true,
+            onTap: () {},
+            badgeContent: Icon(
+              Icons.shopping_cart,
+              color: CustomColors.customSwatchColor,
+              size: 25,
+            ),
+            badgeAnimation: const badges.BadgeAnimation.slide(
+              animationDuration: Duration(milliseconds: 500),
+              colorChangeAnimationDuration: Duration(seconds: 1),
+              loopAnimation: false,
+              curve: Curves.fastOutSlowIn,
+              colorChangeAnimationCurve: Curves.easeInCubic,
+            ),
+            badgeStyle: const badges.BadgeStyle(
+              padding: EdgeInsets.all(5),
+            ),
+          ),
+        ],
       ),
-
-
-      
 
 //CAMPO DE PESQUISA
 
