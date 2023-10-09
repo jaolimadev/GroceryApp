1cc679e3a6f8ca5bbbaaf5e3fc966e9685de0ac9
João Carlos Castro Mendes De Lima <123jccml@gmail.com>
2023-10-09 12:54:25 -0300

    Add 'add to cart' button

--- farmco/lib/src/pages/home/components/item_tile.dart
+++ farmco/lib/src/pages/home/components/item_tile.dart
@@ -14,51 +14,80 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      shadowColor: Colors.grey[300],
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //IMAGEM
-            Expanded(
-              child: Image.asset(item.imgUrl),
-            ),
-
-            //NOME
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            //PREÇO - UNIDADE DE MEDIDA
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 3,
+          shadowColor: Colors.grey[300],
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                //IMAGEM
+                Expanded(
+                  child: Image.asset(item.imgUrl),
+                ),
+
+                //NOME
                 Text(
-                  utilServices.priceToCurency(item.price),
-                  style:  TextStyle(
-                    fontSize: 20,
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color: CustomColors.customSwatchColor,
                   ),
                 ),
 
-                Text('/${item.unit}', style: TextStyle(
-                  color: Colors.grey[500],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                ),)
+                //PREÇO - UNIDADE DE MEDIDA
+                Row(
+                  children: [
+                    Text(
+                      utilServices.priceToCurency(item.price),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                ),
               ],
-
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(15),
+                )
+          
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
